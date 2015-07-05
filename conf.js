f5813985b5730f0a7b5ea2d8ee4c28e43ca9fa73
Midhul Varma <midhul.v@gmail.com>
2015-07-05 23:54:11 +0530

    change get

---
+++ conf.js
@@ -0,0 +1,8 @@
+// Configuration file for gui tracker
+
+conf = {
+    'base_port': 7000,
+    'rows': 10,
+    'cols': 10,
+    'gap': 70
+};
--- get.pl
+++ get.pl
@@ -1,8 +1,16 @@
 :- use_module(library(http/http_open)).
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_server_files)).
 
-      
+:- use_module(library(http/http_path)).
+
+:- multifile user:file_search_path/2.
+
+user:file_search_path(pwd, Cwd) :- working_directory(Cwd, Cwd).
+user:file_search_path(files, pwd(files)).
+
+http:location(files, root(files), []).
 
 get :- 
     http_open('http://localhost:8000/test.zip', In, []),
@@ -12,4 +20,4 @@ get :-
 
 serve :- 
     http_server(http_dispatch, [port(8000)]),
-    http_handler('/test.zip', http_reply_file('test.zip', []), []).
+    http_handler(files(.), serve_files_in_directory(files), [prefix]).
